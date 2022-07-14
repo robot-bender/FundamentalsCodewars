c3b483553c98f0915c8732d5a1d6919431d01df1
kjaniak <krystian.janiak@capgemini.com>
2022-07-14 23:51:28 +0200

    nowy_jakis4

---
+++ src/com/java/Kata1.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Kata1 {
+
+
+    public Kata1() {
+    }
+
+    public int sum(int[] data) {
+        return 0;
+    }
+
+
+}
--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -3,6 +3,13 @@ package com.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Kata1 kata1 = new Kata1();
+        int[] number = new int[5];
+        for (int i = 0; i < number.length; i++) {
+            number[i] = i;
+        }
+        int numerek = kata1.sum(number);
+        System.out.println(numerek);
+        System.out.println(number.length);
     }
 }
